262495c9be3e11ececfb93a7bac1376059f2fa46
Pascal Birchler <pascalb@google.com>
2023-03-28 20:16:18 +0200

    wip 17

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -20,7 +20,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __DIR__, 2 ) . '/preferred-languages.php';
+	require dirname( __DIR__, 2 ) . '/media-experiments.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
