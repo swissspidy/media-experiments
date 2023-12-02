2c9878be3c16f39e2474eb2ef95fc2d8f2b556e6
Pascal Birchler <pascalb@google.com>
2023-12-02 16:59:39 +0100

    e2e: test all formats

--- tests/e2e/specs/imageBlock.spec.ts
+++ tests/e2e/specs/imageBlock.spec.ts
@@ -1,4 +1,15 @@
+import type { ImageFormat } from '@mexp/upload-media';
+
 import { test, expect } from '../fixtures';
+
+const scenarios: Record< ImageFormat, string > = {
+	'jpeg-browser': 'image/jpeg',
+	'webp-browser': 'image/webp',
+	'webp-ffmpeg': 'image/webp',
+	'jpeg-vips': 'image/jpeg',
+	'jpeg-mozjpeg': 'image/jpeg',
+	avif: 'image/avif',
+};
 
 test.describe( 'Image block', () => {
 	test.beforeAll( async ( { requestUtils } ) => {
@@ -9,46 +20,70 @@ test.describe( 'Image block', () => {
 		await admin.createNewPost();
 	} );
 
-	test( 'uploads a file and allows optimizing it afterwards', async ( {
-		page,
-		editor,
-		mediaUtils,
-	} ) => {
-		await editor.insertBlock( { name: 'core/image' } );
+	test.describe( 'uploads a file and allows optimizing it afterwards', () => {
+		for ( const [ preference, expectedMimeType ] of Object.entries(
+			scenarios
+		) ) {
+			test( `uses ${ preference } to convert to ${ expectedMimeType }`, async ( {
+				page,
+				editor,
+				mediaUtils,
+			} ) => {
+				await page.evaluate( ( pref ) => {
+					window.wp.data
+						.dispatch( 'core/preferences' )
+						.set(
+							'media-experiments/preferences',
+							'imageFormat',
+							pref
+						);
+				}, preference );
 
-		const imageBlock = editor.canvas.locator(
-			'role=document[name="Block: Image"i]'
-		);
-		await expect( imageBlock ).toBeVisible();
+				await editor.insertBlock( { name: 'core/image' } );
 
-		await mediaUtils.upload(
-			imageBlock.locator( 'data-testid=form-file-upload-input' )
-		);
+				const imageBlock = editor.canvas.locator(
+					'role=document[name="Block: Image"i]'
+				);
+				await expect( imageBlock ).toBeVisible();
 
-		const settingsPanel = page
-			.getByRole( 'region', {
-				name: 'Editor settings',
-			} )
-			.getByRole( 'tabpanel', {
-				name: 'Settings',
+				await mediaUtils.upload(
+					imageBlock.locator( 'data-testid=form-file-upload-input' )
+				);
+
+				const settingsPanel = page
+					.getByRole( 'region', {
+						name: 'Editor settings',
+					} )
+					.getByRole( 'tabpanel', {
+						name: 'Settings',
+					} );
+
+				await expect( settingsPanel ).toHaveText(
+					new RegExp( `/Mime type: ${ expectedMimeType }/` )
+				);
+				await expect(
+					settingsPanel.getByLabel( '#696969' )
+				).toBeVisible();
+
+				await page.getByRole( 'button', { name: 'Optimize' } ).click();
+
+				const dialog = page.getByRole( 'dialog', {
+					name: 'Compare media quality',
+				} );
+
+				await expect( dialog ).toBeVisible();
+
+				await dialog
+					.getByRole( 'button', { name: 'Use optimized version' } )
+					.click();
+
+				await expect( settingsPanel ).toHaveText(
+					/Mime type: image\/jpeg/
+				);
+				await expect(
+					settingsPanel.getByLabel( '#696969' )
+				).toBeVisible();
 			} );
-
-		await expect( settingsPanel ).toHaveText( /Mime type: image\/png/ );
-		await expect( settingsPanel.getByLabel( '#696969' ) ).toBeVisible();
-
-		await page.getByRole( 'button', { name: 'Optimize' } ).click();
-
-		const dialog = page.getByRole( 'dialog', {
-			name: 'Compare media quality',
-		} );
-
-		await expect( dialog ).toBeVisible();
-
-		await dialog
-			.getByRole( 'button', { name: 'Use optimized version' } )
-			.click();
-
-		await expect( settingsPanel ).toHaveText( /Mime type: image\/webp/ );
-		await expect( settingsPanel.getByLabel( '#696969' ) ).toBeVisible();
+		}
 	} );
 } );
