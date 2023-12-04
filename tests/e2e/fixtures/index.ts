bf8bbc1d7d7c84ae732e99db5b81d03c8b829690
Pascal Birchler <pascalb@google.com>
2023-12-04 19:33:07 +0000

    Collect coverage in e2e tests (#235)

--- tests/e2e/fixtures/index.ts
+++ tests/e2e/fixtures/index.ts
@@ -1,3 +1,5 @@
+import { addCoverageReport } from 'monocart-reporter';
+
 import { test as base } from '@wordpress/e2e-test-utils-playwright';
 
 import { MediaUtils } from './mediaUtils';
@@ -7,6 +9,32 @@ type E2EFixture = {
 };
 
 export const test = base.extend< E2EFixture, {} >( {
+	page: async ( { page, browserName }, use, testInfo ) => {
+		if ( browserName !== 'chromium' || ! process.env.COLLECT_COVERAGE ) {
+			return use( page );
+		}
+
+		await Promise.all( [
+			page.coverage.startJSCoverage( {
+				resetOnNavigation: false,
+			} ),
+			page.coverage.startCSSCoverage( {
+				resetOnNavigation: false,
+			} ),
+		] );
+
+		await use( page );
+
+		const [ jsCoverage, cssCoverage ] = await Promise.all( [
+			page.coverage.stopJSCoverage(),
+			page.coverage.stopCSSCoverage(),
+		] );
+		const coverageList = [ ...jsCoverage, ...cssCoverage ];
+		await addCoverageReport( coverageList, test.info(), {
+			lcov: true,
+			toIstanbul: true,
+		} );
+	},
 	mediaUtils: async ( { page }, use ) => {
 		await use( new MediaUtils( { page } ) );
 	},
--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,9 +1,40 @@
+import { join, resolve } from 'node:path';
+
+import type { CoverageReportOptions } from 'monocart-reporter';
+
 import { defineConfig, devices } from '@playwright/test';
 
-const baseConfig = require( '@wordpress/scripts/config/playwright.config' );
+import baseConfig from '@wordpress/scripts/config/playwright.config';
 
 const config = defineConfig( {
 	...baseConfig,
+	reporter: [
+		...baseConfig.reporter,
+		process.env.COLLECT_COVERAGE === 'true' && [
+			'monocart-reporter',
+			/** @type {CoverageReportOptions} **/
+			{
+				outputFile: './artifacts/e2e-coverage/report.html',
+				logging: 'off',
+				foo: 'bar',
+				coverage: {
+					entryFilter: ( entry: any ) => {
+						return (
+							entry.url.startsWith( 'blob:' ) ||
+							entry.url.includes(
+								'media-experiments/build/media-experiments.js'
+							)
+						);
+					},
+					sourcePath: ( sourcePath: string ) => {
+						return sourcePath.replace( 'media-experiments/', '' );
+					},
+					toIstanbul: true,
+					lcov: true,
+				},
+			},
+		],
+	].filter( Boolean ),
 	projects: [
 		{
 			name: 'chromium',
