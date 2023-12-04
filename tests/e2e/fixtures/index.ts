b2082ec936120c44716a0c670543db4087f0fdf6
Pascal Birchler <pascalb@google.com>
2023-12-04 12:33:16 +0100

    Collect coverage in e2e tests

--- tests/e2e/fixtures/index.ts
+++ tests/e2e/fixtures/index.ts
@@ -1,3 +1,5 @@
+import { mixinFixtures as mixinCoverage } from '@bgotink/playwright-coverage';
+
 import { test as base } from '@wordpress/e2e-test-utils-playwright';
 
 import { MediaUtils } from './mediaUtils';
@@ -6,10 +8,12 @@ type E2EFixture = {
 	mediaUtils: MediaUtils;
 };
 
-export const test = base.extend< E2EFixture, {} >( {
-	mediaUtils: async ( { page }, use ) => {
-		await use( new MediaUtils( { page } ) );
-	},
-} );
+export const test = mixinCoverage(
+	base.extend< E2EFixture, {} >( {
+		mediaUtils: async ( { page }, use ) => {
+			await use( new MediaUtils( { page } ) );
+		},
+	} )
+);
 
 export { expect } from '@wordpress/e2e-test-utils-playwright';
--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,9 +1,49 @@
+import { join, resolve } from 'node:path';
+
 import { defineConfig, devices } from '@playwright/test';
 
-const baseConfig = require( '@wordpress/scripts/config/playwright.config' );
+import baseConfig from '@wordpress/scripts/config/playwright.config';
 
 const config = defineConfig( {
 	...baseConfig,
+	reporter: [
+		...baseConfig.reporter,
+		process.env.COLLECT_COVERAGE === 'true' && [
+			'@bgotink/playwright-coverage',
+			/** @type {import('@bgotink/playwright-coverage').CoverageReporterOptions} */ {
+				sourceRoot: process.cwd(),
+				exclude: [
+					'**/**.svg',
+					'**/webpack/**',
+					'**/external window**',
+				],
+				resultDir: join( process.cwd(), 'artifacts/e2e-coverage' ),
+				rewritePath: ( { relativePath }: { relativePath: string } ) => {
+					return resolve(
+						process.cwd(),
+						relativePath.replace( 'media-experiments/', './' )
+					);
+				},
+				reports: [
+					[ 'html' ],
+					[
+						'lcovonly',
+						{
+							file: 'coverage.lcov',
+						},
+					],
+					[
+						'text-summary',
+						{
+							file: null,
+						},
+					],
+				],
+				// See https://github.com/istanbuljs/nyc#high-and-low-watermarks
+				// watermarks: {},
+			},
+		],
+	].filter( Boolean ),
 	projects: [
 		{
 			name: 'chromium',
