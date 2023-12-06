a754d3a1e7cd80acf37af218565a61d4759e0043
Pascal Birchler <pascalb@google.com>
2023-12-06 13:35:52 +0100

    Add MutationObserver for cross-origin isolation

--- packages/edit-post/src/@types/wordpress__edit_post/store/selectors.d.ts
+++ packages/edit-post/src/@types/wordpress__edit_post/store/selectors.d.ts
@@ -2,3 +2,26 @@
  * Returns true if the publish sidebar is opened.
  */
 export function isPublishSidebarOpened(): boolean;
+
+/**
+ * Returns true if meta boxes are initialized.
+ */
+export function areMetaBoxesInitialized(): boolean;
+
+/**
+ * Returns true if the post is using meta boxes.
+ */
+export function hasMetaBoxes(): boolean;
+
+/**
+ * Returns true if a meta box location is active and visible.
+ */
+export function isMetaBoxLocationVisible(
+	state: Record< string, unknown >,
+	location: string
+): boolean;
+
+/**
+ * Returns an array of active meta box locations.
+ */
+export function getActiveMetaBoxLocations(): string[];
--- packages/edit-post/src/crossOriginIsolation/index.ts
+++ packages/edit-post/src/crossOriginIsolation/index.ts
@@ -1,4 +1,6 @@
 import { addFilter } from '@wordpress/hooks';
+import { store as editPostStore } from '@wordpress/edit-post';
+import { select as globalSelect, subscribe } from '@wordpress/data';
 
 type CrossOriginValue = 'anonymous' | 'use-credentials' | '' | undefined;
 
@@ -7,8 +9,113 @@ function forceCrossOrigin( imgCrossOrigin: CrossOriginValue, url: string ) {
 	return 'anonymous' as CrossOriginValue;
 }
 
-addFilter(
-	'media.crossOrigin',
-	'media-experiments/cross-origin-isolation/force-crossorigin',
-	forceCrossOrigin
-);
+if ( window.crossOriginIsolated ) {
+	addFilter(
+		'media.crossOrigin',
+		'media-experiments/cross-origin-isolation/force-crossorigin',
+		forceCrossOrigin
+	);
+
+	/**
+	 * Complementary component to the Cross_Origin_Isolation PHP class
+	 * that detects dynamically added DOM nodes that are missing the `crossorigin` attribute.
+	 * These are typically found in custom meta boxes and the WordPress admin bar.
+	 *
+	 * @return {null} Rendered component
+	 */
+
+	const observer = new MutationObserver( ( mutations ) => {
+		mutations.forEach( ( mutation ) => {
+			[ mutation.addedNodes, mutation.target ].forEach( ( node ) => {
+				if (
+					! ( 'querySelectorAll' in node ) ||
+					! node.querySelectorAll
+				) {
+					return;
+				}
+
+				const elements = [
+					...( node as HTMLElement ).querySelectorAll( 'img' ),
+				];
+				if ( node instanceof HTMLImageElement ) {
+					elements.push( node );
+				}
+
+				elements.forEach( ( el: HTMLImageElement ) => {
+					if ( el.hasAttribute( 'crossorigin' ) ) {
+						return;
+					}
+
+					const imgSrc = new URL( el.src );
+
+					if ( imgSrc.origin !== location.origin ) {
+						el.setAttribute( 'crossorigin', 'anonymous' );
+					}
+				} );
+			} );
+		} );
+	} );
+
+	let prevHasMetaBoxes = false;
+
+	document.querySelectorAll( '#wpadminbar' ).forEach( ( subTree ) => {
+		observer.observe( subTree, {
+			childList: true,
+			attributes: true,
+			subtree: true,
+		} );
+	} );
+
+	async function waitForChildren( element: Element ) {
+		return new Promise< void >( ( resolve ) => {
+			if ( ! element ) {
+				return resolve();
+			}
+
+			if ( element.children.length ) {
+				return resolve();
+			}
+
+			const obs = new MutationObserver( () => {
+				if ( element.children.length ) {
+					observer.disconnect();
+					resolve();
+				}
+			} );
+
+			obs.observe( element, {
+				childList: true,
+				subtree: true,
+			} );
+		} );
+	}
+
+	const unsubscribe = subscribe( async () => {
+		const wrapperEl = document.querySelector(
+			'.edit-post-layout__metaboxes'
+		);
+		const hasMetaBoxes =
+			globalSelect( editPostStore ).areMetaBoxesInitialized() &&
+			globalSelect( editPostStore ).hasMetaBoxes() &&
+			globalSelect( editPostStore )
+				.getActiveMetaBoxLocations()
+				.some( ( location ) =>
+					globalSelect( editPostStore ).isMetaBoxLocationVisible(
+						location
+					)
+				) &&
+			wrapperEl !== null;
+
+		if ( hasMetaBoxes && ! prevHasMetaBoxes ) {
+			prevHasMetaBoxes = hasMetaBoxes;
+			unsubscribe();
+
+			await waitForChildren( wrapperEl );
+
+			observer.observe( wrapperEl, {
+				attributes: true,
+				subtree: true,
+			} );
+		}
+	}, editPostStore );
+}
