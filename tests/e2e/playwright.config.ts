99b1156cc3d6e0762a191766c6ded18d7d222c50
Pascal Birchler <pascalb@google.com>
2023-12-27 23:28:41 +0100

    Remove comment

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -38,7 +38,6 @@ const config = defineConfig( {
 			name: 'chromium',
 			use: { ...devices[ 'Desktop Chrome' ] },
 		},
-		// Probably just disalbe Webkit tests because of
 		{
 			name: 'webkit',
 			use: { ...devices[ 'Desktop Safari' ] },
