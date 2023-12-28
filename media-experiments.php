d5088cc38f9d3a672438b8d86d0d35a79deee26d
Pascal Birchler <pascalb@google.com>
2023-12-28 15:44:21 +0100

    Add primary branch header for git updater

--- media-experiments.php
+++ media-experiments.php
@@ -12,6 +12,7 @@
  * Requires at least: 6.4
  * Requires PHP: 8.0
  * GitHub Plugin URI: https://github.com/swissspidy/media-experiments
+ * Primary Branch: main
  *
  * @package MediaExperiments
  */
