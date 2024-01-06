4af3e84886f4eec9ca3a58e1819ffa1b95ffef3d
Pascal Birchler <pascalb@google.com>
2024-01-07 00:54:41 +0100

    Some blurhash fixes
    Seems to fix #293

--- packages/upload-media/src/workers/blurhash.ts
+++ packages/upload-media/src/workers/blurhash.ts
@@ -6,7 +6,7 @@ export async function getBlurHash( url: string ) {
 	const bitmap = await createImageBitmap( imgBlob );
 
 	const newWidth = 100;
-	const newHeight = ( 100 / bitmap.width ) * bitmap.height;
+	const newHeight = Math.floor( ( 100 / bitmap.width ) * bitmap.height );
 
 	const canvas = new OffscreenCanvas( newWidth, newHeight );
 	const ctx = canvas.getContext( '2d' );
@@ -24,5 +24,5 @@ export async function getBlurHash( url: string ) {
 	/// Scale down for performance reasons.
 	// See https://github.com/woltapp/blurhash/blob/cb151cab5b7d9cd3eef624e12e30381c6d292f0d/Readme.md#L112
 
-	return encode( imageData.data, imageData.width, imageData.height, 4, 3 );
+	return encode( imageData.data, imageData.width, imageData.height, 5, 5 );
 }
