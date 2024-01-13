b220b0abea6f4f61713d04119eed55e1c1600636
Pascal Birchler <pascalb@google.com>
2024-01-13 10:35:04 +0100

    Update thumbnail tests

--- tests/e2e/specs/images/thumbnails.spec.ts
+++ tests/e2e/specs/images/thumbnails.spec.ts
@@ -1,8 +1,39 @@
-import { RestAttachment } from '@mexp/upload-media';
+import {
+	ImageFormat,
+	ImageLibrary,
+	RestAttachment,
+	ThumbnailGeneration,
+} from '@mexp/upload-media';
 
 import { test, expect } from '../../fixtures';
 
-const thumbnailGenerationModes = [ 'server', 'client', 'smart' ];
+const scenarios: {
+	imageLibrary: ImageLibrary;
+	thumbnailGeneration: ThumbnailGeneration;
+}[] = [
+	// imageLibrary doesn't matter when thumbnailGeneration === 'server'
+	{
+		imageLibrary: 'browser',
+		thumbnailGeneration: 'server',
+	},
+	{
+		imageLibrary: 'browser',
+		thumbnailGeneration: 'client',
+	},
+	// Same behavior as browser & client, just added for completeness.
+	{
+		imageLibrary: 'browser',
+		thumbnailGeneration: 'smart',
+	},
+	{
+		imageLibrary: 'vips',
+		thumbnailGeneration: 'client',
+	},
+	{
+		imageLibrary: 'vips',
+		thumbnailGeneration: 'smart',
+	},
+];
 
 test.describe( 'Images', () => {
 	test.beforeAll( async ( { requestUtils } ) => {
@@ -10,121 +41,110 @@ test.describe( 'Images', () => {
 	} );
 
 	test.describe( 'Thumbnail generation', () => {
-		for ( const thumbnailGeneration of thumbnailGenerationModes ) {
-			test(
-				thumbnailGeneration,
-				async ( {
-					admin,
-					page,
-					editor,
-					mediaUtils,
-					requestUtils,
-					browserName,
-				} ) => {
-					test.skip(
-						browserName === 'webkit',
-						'No cross-origin isolation in Playwright WebKit builds yet, see https://github.com/microsoft/playwright/issues/14043'
-					);
+		for ( const { imageLibrary, thumbnailGeneration } of scenarios ) {
+			test( `uses ${ imageLibrary } and ${ thumbnailGeneration } mode to generate thumbnails`, async ( {
+				admin,
+				page,
+				editor,
+				mediaUtils,
+				requestUtils,
+				browserName,
+			} ) => {
+				test.skip(
+					browserName === 'webkit',
+					'No cross-origin isolation in Playwright WebKit builds yet, see https://github.com/microsoft/playwright/issues/14043'
+				);
 
-					await admin.createNewPost();
+				await admin.createNewPost();
 
-					await page.evaluate(
-						( [ mode ] ) => {
-							window.wp.data
-								.dispatch( 'core/preferences' )
-								.set(
-									'media-experiments/preferences',
-									'optimizeOnUpload',
-									false
-								);
-							window.wp.data
-								.dispatch( 'core/preferences' )
-								.set(
-									'media-experiments/preferences',
-									'jpeg_outputFormat',
-									'jpeg'
-								);
-							window.wp.data
-								.dispatch( 'core/preferences' )
-								.set(
-									'media-experiments/preferences',
-									'imageLibrary',
-									'browser'
-								);
-							window.wp.data
-								.dispatch( 'core/preferences' )
-								.set(
-									'media-experiments/preferences',
-									'thumbnailGeneration',
-									mode
-								);
-						},
-						[ thumbnailGeneration ]
-					);
+				await page.evaluate(
+					( [ lib, mode ] ) => {
+						window.wp.data
+							.dispatch( 'core/preferences' )
+							.set(
+								'media-experiments/preferences',
+								'optimizeOnUpload',
+								false
+							);
+						window.wp.data
+							.dispatch( 'core/preferences' )
+							.set(
+								'media-experiments/preferences',
+								'jpeg_outputFormat',
+								'jpeg'
+							);
+						window.wp.data
+							.dispatch( 'core/preferences' )
+							.set(
+								'media-experiments/preferences',
+								'imageLibrary',
+								lib
+							);
+						window.wp.data
+							.dispatch( 'core/preferences' )
+							.set(
+								'media-experiments/preferences',
+								'thumbnailGeneration',
+								mode
+							);
+					},
+					[ imageLibrary, thumbnailGeneration ]
+				);
 
-					await editor.insertBlock( { name: 'core/image' } );
+				await editor.insertBlock( { name: 'core/image' } );
 
-					const imageBlock = editor.canvas.locator(
-						'role=document[name="Block: Image"i]'
-					);
-					await expect( imageBlock ).toBeVisible();
+				const imageBlock = editor.canvas.locator(
+					'role=document[name="Block: Image"i]'
+				);
+				await expect( imageBlock ).toBeVisible();
 
-					await mediaUtils.upload(
-						imageBlock.locator(
-							'data-testid=form-file-upload-input'
-						),
-						'tree-on-water-1200x800.jpeg'
-					);
+				await mediaUtils.upload(
+					imageBlock.locator( 'data-testid=form-file-upload-input' ),
+					'tree-on-water-1200x800.jpeg'
+				);
 
-					await page.waitForFunction(
-						() =>
-							window.wp.data
-								.select( 'media-experiments/upload' )
-								.getItems().length === 0,
-						undefined,
-						{
-							timeout: 20000, // Transcoding might take longer
-						}
-					);
+				await page.waitForFunction(
+					() =>
+						window.wp.data
+							.select( 'media-experiments/upload' )
+							.getItems().length === 0,
+					undefined,
+					{
+						timeout: 20000, // Transcoding might take longer
+					}
+				);
 
-					const imageId = await page.evaluate(
-						() =>
-							window.wp.data
-								.select( 'core/block-editor' )
-								.getSelectedBlock()?.attributes?.id
-					);
+				const imageId = await page.evaluate(
+					() =>
+						window.wp.data
+							.select( 'core/block-editor' )
+							.getSelectedBlock()?.attributes?.id
+				);
 
-					const media: RestAttachment = await requestUtils.rest( {
-						method: 'GET',
-						path: `/wp/v2/media/${ imageId }`,
-					} );
+				const media: RestAttachment = await requestUtils.rest( {
+					method: 'GET',
+					path: `/wp/v2/media/${ imageId }`,
+				} );
 
-					expect( media.media_details.sizes ).toHaveProperty(
-						'thumbnail'
-					);
-					expect( media.media_details.sizes ).toHaveProperty(
-						'medium'
-					);
-					expect( media.media_details.sizes ).toHaveProperty(
-						'medium_large'
-					);
-					expect( media.media_details.sizes ).toHaveProperty(
-						'large'
-					);
-					expect( media.media_details.sizes ).toHaveProperty(
-						'full'
-					);
+				expect( media.media_details.sizes ).toHaveProperty(
+					'thumbnail'
+				);
+				expect( media.media_details.sizes ).toHaveProperty( 'medium' );
+				expect( media.media_details.sizes ).toHaveProperty(
+					'medium_large'
+				);
+				expect( media.media_details.sizes ).toHaveProperty( 'large' );
+				expect( media.media_details.sizes ).toHaveProperty( 'full' );
 
-					expect(
-						await mediaUtils.getImageBuffer(
-							// @ts-ignore
-							media.media_details.sizes.thumbnail.source_url
-						)
-					).toMatchSnapshot(
-						`thumbnail-generation-${ thumbnailGeneration }.jpeg`
-					);
-				}
-			);
+				expect(
+					await mediaUtils.getImageBuffer(
+						// @ts-ignore
+						media.media_details.sizes.thumbnail.source_url
+					)
+				).toMatchSnapshot(
+					`thumbnail-generation-${ imageLibrary }-${ thumbnailGeneration }.jpeg`
+				);
+			} );
 		}
 	} );
 } );
