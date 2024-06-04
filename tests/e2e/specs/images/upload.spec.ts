bf4f563d501d6d92848756a6216079ba9d4c185d
Pascal Birchler <pascalb@google.com>
2024-06-04 21:12:12 +0200

    Split up test files

--- tests/e2e/specs/images/upload.spec.ts
+++ tests/e2e/specs/images/upload.spec.ts
@@ -1,15 +1,55 @@
-import { ImageLibrary, RestAttachment } from '@mexp/upload-media';
+import {
+	ImageFormat,
+	ImageLibrary,
+	RestAttachment,
+} from '@mexp/upload-media';
 
 import { test, expect } from '../../fixtures';
 
 const scenarios: {
 	imageLibrary: ImageLibrary;
+	outputFormat: ImageFormat;
+	expectedMimeType: string;
 }[] = [
 	{
 		imageLibrary: 'browser',
+		outputFormat: 'jpeg',
+		expectedMimeType: 'image/jpeg',
+	},
+	{
+		imageLibrary: 'browser',
+		outputFormat: 'webp',
+		expectedMimeType: 'image/webp',
+	},
+	{
+		imageLibrary: 'browser',
+		outputFormat: 'png',
+		expectedMimeType: 'image/png',
+	},
+	{
+		imageLibrary: 'browser',
+		outputFormat: 'avif',
+		expectedMimeType: 'image/avif',
 	},
 	{
 		imageLibrary: 'vips',
+		outputFormat: 'jpeg',
+		expectedMimeType: 'image/jpeg',
+	},
+	{
+		imageLibrary: 'vips',
+		outputFormat: 'webp',
+		expectedMimeType: 'image/webp',
+	},
+	{
+		imageLibrary: 'vips',
+		outputFormat: 'png',
+		expectedMimeType: 'image/png',
+	},
+	{
+		imageLibrary: 'vips',
+		outputFormat: 'avif',
+		expectedMimeType: 'image/avif',
 	},
 ];
 
@@ -18,9 +58,13 @@ test.describe( 'Images', () => {
 		await requestUtils.deleteAllMedia();
 	} );
 
-	test.describe( 'Big image size threshold', () => {
-		for ( const { imageLibrary } of scenarios ) {
-			test( `does not upscale when using ${ imageLibrary } library`, async ( {
+	test.describe( 'Upload', () => {
+		for ( const {
+			imageLibrary,
+			outputFormat,
+			expectedMimeType,
+		} of scenarios ) {
+			test( `uses ${ outputFormat }@${ imageLibrary } to convert to ${ expectedMimeType }`, async ( {
 				admin,
 				page,
 				editor,
@@ -29,14 +73,25 @@ test.describe( 'Images', () => {
 				requestUtils,
 			} ) => {
 				test.skip(
-					browserName === 'webkit' && imageLibrary === 'vips',
+					browserName === 'webkit' &&
+						( imageLibrary === 'vips' || outputFormat === 'avif' ),
 					'No cross-origin isolation in Playwright WebKit builds yet, see https://github.com/microsoft/playwright/issues/14043'
+				);
+
+				test.skip(
+					browserName === 'webkit' && outputFormat === 'webp',
+					'WebKit does not currently support Canvas.toBlob with WebP'
+				);
+
+				test.skip(
+					browserName === 'firefox' && outputFormat === 'avif',
+					'AVIF encoding on Firefox is very slow'
 				);
 
 				await admin.createNewPost();
 
 				await page.evaluate(
-					( [ lib ] ) => {
+					( [ lib, fmt ] ) => {
 						window.wp.data
 							.dispatch( 'core/preferences' )
 							.set(
@@ -49,7 +104,7 @@ test.describe( 'Images', () => {
 							.set(
 								'media-experiments/preferences',
 								'jpeg_outputFormat',
-								'jpeg'
+								fmt
 							);
 						window.wp.data
 							.dispatch( 'core/preferences' )
@@ -70,10 +125,10 @@ test.describe( 'Images', () => {
 							.set(
 								'media-experiments/preferences',
 								'bigImageSizeThreshold',
-								3000
+								1140
 							);
 					},
-					[ imageLibrary ]
+					[ imageLibrary, outputFormat ]
 				);
 
 				await editor.insertBlock( { name: 'core/image' } );
@@ -95,7 +150,7 @@ test.describe( 'Images', () => {
 							.getItems().length === 0,
 					undefined,
 					{
-						timeout: 30_000, // Transcoding might take longer
+						timeout: 60_000, // Transcoding might take longer, especially AVIF
 					}
 				);
 
@@ -107,7 +162,9 @@ test.describe( 'Images', () => {
 				);
 
 				// See https://github.com/swissspidy/media-experiments/issues/321.
-				expect( imageUrl ).toMatch( /-1024x683/ );
+				expect( imageUrl ).toMatch(
+					new RegExp( `-1024x683.${ outputFormat }\$` )
+				);
 
 				const imageId = await page.evaluate(
 					() =>
@@ -124,12 +181,14 @@ test.describe( 'Images', () => {
 				/* eslint-disable camelcase */
 				expect( media.media_details ).toEqual(
 					expect.objectContaining( {
-						width: 1200,
-						height: 800,
+						width: 1140,
+						height: 760,
 						filesize: expect.any( Number ),
 						blurhash: expect.any( String ),
 						dominant_color: expect.any( String ),
-						has_transparency: false,
+						// TODO: Explicitly look for has_transparency: false
+						// See https://github.com/swissspidy/media-experiments/issues/492
+						has_transparency: expect.any( Boolean ),
 						image_meta: expect.anything(),
 						sizes: expect.anything(),
 					} )
@@ -140,80 +199,104 @@ test.describe( 'Images', () => {
 							width: 150,
 							height: 150,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-150x150.jpeg' ),
-							source_url:
-								expect.stringContaining( '-150x150.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-150x150.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-150x150.${ outputFormat }`
+							),
 						} ),
 						medium: expect.objectContaining( {
 							width: 300,
 							height: 200,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-300x200.jpeg' ),
-							source_url:
-								expect.stringContaining( '-300x200.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-300x200.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-300x200.${ outputFormat }`
+							),
 						} ),
 						medium_large: expect.objectContaining( {
 							width: 768,
 							height: 512,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-768x512.jpeg' ),
-							source_url:
-								expect.stringContaining( '-768x512.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-768x512.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-768x512.${ outputFormat }`
+							),
 						} ),
 						large: expect.objectContaining( {
 							width: 1024,
 							height: 683,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-1024x683.jpeg' ),
-							source_url:
-								expect.stringContaining( '-1024x683.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-1024x683.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-1024x683.${ outputFormat }`
+							),
 						} ),
 
 						'bottom-right': expect.objectContaining( {
 							width: 220,
 							height: 220,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-220x220.jpeg' ),
-							source_url:
-								expect.stringContaining( '-220x220.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-220x220.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-220x220.${ outputFormat }`
+							),
 						} ),
 						'custom-size': expect.objectContaining( {
 							width: 100,
 							height: 100,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-100x100.jpeg' ),
-							source_url:
-								expect.stringContaining( '-100x100.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-100x100.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-100x100.${ outputFormat }`
+							),
 						} ),
 						'ninek-height': expect.objectContaining( {
 							width: 400,
 							height: 267,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-400x267.jpeg' ),
-							source_url:
-								expect.stringContaining( '-400x267.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-400x267.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-400x267.${ outputFormat }`
+							),
 						} ),
 
 						'ninek-width': expect.objectContaining( {
 							width: 900,
 							height: 600,
 							filesize: expect.any( Number ),
-							mime_type: 'image/jpeg',
-							file: expect.stringContaining( '-900x600.jpeg' ),
-							source_url:
-								expect.stringContaining( '-900x600.jpeg' ),
+							mime_type: expectedMimeType,
+							file: expect.stringContaining(
+								`-900x600.${ outputFormat }`
+							),
+							source_url: expect.stringContaining(
+								`-900x600.${ outputFormat }`
+							),
 						} ),
 						full: expect.objectContaining( {
-							width: 1200,
-							height: 800,
-							mime_type: 'image/jpeg',
+							width: 1140,
+							height: 760,
+							mime_type: expectedMimeType,
 						} ),
 					} )
 				);
