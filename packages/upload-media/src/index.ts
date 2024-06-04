f10dda156e84cecce1738293882bc88ee08b44ed
Pascal Birchler <pascalb@google.com>
2024-06-04 09:27:04 +0200

    Remove now outdated comments

--- packages/upload-media/src/index.ts
+++ packages/upload-media/src/index.ts
@@ -36,48 +36,10 @@ export type {
 export { uploadStore as store, UploadError };
 
 /*
- Try to get dimensions and poster for placeholder resources.
- This way we can show something more meaningful to the user before transcoding has finished.
- Since this uses ffmpeg, we're going to limit this to one at a time.
-
- For pending video items without a poster still, use FFmpeg to generate a poster.
- This way we can show something more meaningful to the user before transcoding has finished.
- Since this uses FFmpeg, we're going to limit this to one at a time.
-
- TODO: Generate poster with FFmpeg if missing.
- Could be after converting gif or similar.
- Update poster in video block (should revoke temp blob URL)
- When video upload finishes, also upload poster image.
-
- Set temporary URL to create placeholder media file, this is replaced
- with final file from media gallery when upload is `done` below.
- TODO: remove in favor of logic below.
-*/
-
-// Move to uploadItem action
-// subscribe( () => {
-// 	const items: QueueItem[] = select( uploadStore ).getUploadedItems();
-//
-// 	for ( const item of items ) {
-// 		const { id, onChange, onSuccess, onBatchSuccess, attachment, batchId } =
-// 			item;
-// 		if ( attachment ) {
-// 			onChange?.( [ attachment ] );
-// 			onSuccess?.( [ attachment ] );
-// 		}
-// 		if ( batchId && select( uploadStore ).isBatchUploaded( batchId ) ) {
-// 			onBatchSuccess?.();
-// 		}
-// 		void dispatch( uploadStore ).completeItem( id );
-// 	}
-// }, uploadStore );
-
-/*
  The WordPress REST API requires passing term IDs instead of slugs.
  We are storing them here in a simple slug => id map so that we can
  still reference them by slug to make things a bit easier.
 */
-
 void dispatch( uploadStore ).setMediaSourceTerms(
 	window.mediaExperiments.mediaSourceTerms
 );
