65ca80ab32e51014c8d4c7b4cfaf1ca379c16577
Pascal Birchler <pascalb@google.com>
2024-06-09 18:28:56 +0200

    Add custom `Update URI`
    See #504

--- media-experiments.php
+++ media-experiments.php
@@ -1,19 +1,17 @@
 <?php
 /**
- * Plugin Name: Media Experiments
- * Plugin URI:  https://github.com/swissspidy/media-experiments
- * Description: Media Experiments
- * Version:     0.0.2
- * Author:      Pascal Birchler
- * Author URI:  https://pascalbirchler.com
- * License:     GPLv2 or later
- * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * Text Domain: media-experiments
+ * Plugin Name:       Media Experiments
+ * Plugin URI:        https://github.com/swissspidy/media-experiments
+ * Description:       Media Experiments
+ * Version:           0.0.2
+ * Author:            Pascal Birchler
+ * Author URI:        https://pascalbirchler.com
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain:       media-experiments
  * Requires at least: 6.5
- * Requires PHP: 8.0
- * GitHub Plugin URI: https://github.com/swissspidy/media-experiments
- * Primary Branch: main
- * Release Asset: true
+ * Requires PHP:      8.0
+ * Update URI:        https://swissspidy.github.io/media-experiments/update.json
  *
  * @package MediaExperiments
  */
