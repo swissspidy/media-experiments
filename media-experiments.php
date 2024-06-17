f1c94493e9c238e0ca15a5cf13249d3b34bbfc10
Pascal Birchler <pascalb@google.com>
2024-06-17 13:36:58 +0200

    Use date-based versioning for nightlies

--- media-experiments.php
+++ media-experiments.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Media Experiments
  * Plugin URI:        https://github.com/swissspidy/media-experiments
  * Description:       Media Experiments
- * Version:           0.0.3
+ * Version:           0.1.2024.6.17
  * Author:            Pascal Birchler
  * Author URI:        https://pascalbirchler.com
  * License:           GPLv2 or later
