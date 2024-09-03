8f106f854f73ee4191caa0c9a35866fada26c6f1
Pascal Birchler <pascalb@google.com>
2024-09-03 23:10:45 +0200

    Add a test

---
+++ tests/e2e/specs/images/jxl.spec.ts
@@ -0,0 +1,87 @@
+/**
+ * Internal dependencies
+ */
+import { expect, test } from '../../fixtures';
+
+test.describe( 'Images', () => {
+	test.beforeAll( async ( { requestUtils } ) => {
+		await Promise.all( [
+			requestUtils.deleteAllMedia(),
+			requestUtils.resetPreferences(),
+		] );
+	} );
+
+	test( 'uploads and converts a JPEG XL image', async ( {
+		browserName,
+		admin,
+		page,
+		editor,
+		mediaUtils,
+	} ) => {
+		test.skip(
+			browserName === 'webkit',
+			'Needs some investigation as to why image is uploaded as PNG instead of JPEG'
+		);
+
+		await admin.createNewPost();
+
+		await page.evaluate( () => {
+			window.wp.data
+				.dispatch( 'core/preferences' )
+				.set( 'media-experiments/preferences', 'imageFormat', 'jpeg' );
+			window.wp.data
+				.dispatch( 'core/preferences' )
+				.set(
+					'media-experiments/preferences',
+					'imageLibrary',
+					'browser'
+				);
+		} );
+
+		await editor.insertBlock( { name: 'core/image' } );
+
+		const imageBlock = editor.canvas.locator(
+			'role=document[name="Block: Image"i]'
+		);
+		await expect( imageBlock ).toBeVisible();
+
+		await mediaUtils.upload(
+			imageBlock.locator( 'data-testid=form-file-upload-input' ),
+			'hill-800x600.jxl'
+		);
+
+		await expect(
+			page
+				.getByRole( 'button', { name: 'Dismiss this notice' } )
+				.filter( {
+					hasText:
+						/Sorry, you are not allowed to upload this file type/,
+				} )
+		).toBeHidden();
+
+		await page.waitForFunction(
+			() =>
+				window.wp.data.select( 'media-experiments/upload' ).getItems()
+					.length === 0,
+			undefined,
+			{
+				timeout: 30_000,
+			}
+		);
+
+		const settingsPanel = page
+			.getByRole( 'region', {
+				name: 'Editor settings',
+			} )
+			.getByRole( 'tabpanel', {
+				name: 'Settings',
+			} );
+
+		await expect( settingsPanel ).toHaveText( /Mime type: image\/jpeg/ );
+		await expect(
+			settingsPanel.getByLabel( /#837776|#827675/ )
+		).toBeVisible();
+
+		await expect( page.locator( 'css=[data-blurhash]' ) ).toBeVisible();
+	} );
+} );
