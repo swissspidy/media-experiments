de10c5503982a9567c0e59e226d767b764c73e9a
Pascal Birchler <pascalb@google.com>
2025-09-01 19:43:39 +0200

    Require WordPress 6.7+

--- media-experiments.php
+++ media-experiments.php
@@ -9,7 +9,7 @@
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Text Domain:       media-experiments
- * Requires at least: 6.6
+ * Requires at least: 6.7
  * Requires PHP:      8.0
  * Update URI:        https://swissspidy.github.io/media-experiments/update.json
  *
